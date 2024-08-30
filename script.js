9fac21c9aec51a5f7671f9731af64a5e66f2583b
Alleyhustler <153684384+Alleyhustler@users.noreply.github.com>
2024-08-30 21:43:37 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById("enterButton").addEventListener("click", function() {
+    var links = document.getElementById("links");
+    links.style.display = "block"; // Show the links when button is clicked
+});
